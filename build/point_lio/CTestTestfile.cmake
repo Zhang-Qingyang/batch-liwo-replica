d316aa73a09b0e0035c3b3be1aaae7a9931d4e35
张庆杨 <miscedences@gmail.com>
2025-12-03 07:38:11 +0800

    更新readme

--- build/point_lio/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zhangqingyang/point_lio_ros2
-# Build directory: /home/zhangqingyang/point_lio_ros2/build/point_lio
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
